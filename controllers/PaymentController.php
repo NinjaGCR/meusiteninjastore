5f54637d16dd2bd607b2d1d21233d59956953523
NinjaGCR <NinjaGCR@github.com>
2020-05-23 17:36:43 -0300

    Initial commit

---
+++ controllers/PaymentController.php
@@ -0,0 +1,7 @@
+<?php
+
+$token = $_REQUEST["token"];
+$payment_method_id = $_REQUEST["payment_method_id"];
+$installments = $_REQUEST["installments"];
+$issuer_id = $_REQUEST["issuer_id"];
+
---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pagamento Inicial</title>
+    <link rel="stylesheet" href="lib/style.css">
+    <link rel="stylesheet" href="lib/rodape.css">
+    <link rel="stylesheet" href="lib/index.css">
+    <link rel="stylesheet" href="lib/main.css">
+    <link rel="stylesheet" href="lib/font-awesome.min.css">
+</head>
+<body>
+<header class="titulo-principal">
+    <h1>NINJASTORE</h1>
+    <p class="subtitulo-principal">Os melhores preços estão aqui</p>
+    <h3 class="fic"></h3>
+</header>
+<header class="sobre-principal">
+    <h1>COMO FUNCIONA?</h1>
+    <p class="sobre-subtitulo"><strong>Entrega</strong></p>
+    <p class="args">Após a confirmação do pagamento, voce receberá</p>
+    <p class="args">um email com o link para download do plugin e a licensa</p>
+    <p class="sobre-subtitulo"><strong>Encomendas</strong></p>
+    <p class="args">Gostaria de um plugin proprio para seu servidor?</p>
+    <p class="args">entre em contato conosco contato@ninjatore.tk</p>
+    <h3 class="fic"></h3>
+</header>
+<header class="seguranca-atualizacao">
+    <p strong>&#8227; Segurança</strong></p>
+    <p class="args">Após a confirmação do pagamento, voce receberá</p>
+    <h3 class="fic"></h3>
+</header>
+   <div class="product">
+       <div class="product_title">MMobMobes</div>
+       <div class="product_image"><img src="img/logoallproduct.png" alt=""></div>
+       <div class="product_desc">Plugin para congelar as ações dos mobs</div>
+       <div class="product_btn"><a mp-mode="dftl" href="https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=38603847-0c463b8f-d16e-42b0-94d9-b965c3fc9fec" name="MP-payButton" class='blue-ar-l-rn-none'>Comprar</a></div>
+   </div>
+   <div class="product">
+       <div class="product_title">NPlaca</div>
+       <div class="product_image"><img src="img/logoallproduct.png" alt=""></div>
+       <div class="product_desc">Plugin de evento placa perdida</div>
+       <div class="product_btn"><a mp-mode="dftl" href="https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=38603847-a43713a2-e54f-4b09-8e93-30051d18239c" name="MP-payButton" class='blue-ar-l-rn-none'>Comprar</a></div>
+   </div>
+<div class="teste">
+    <div class="product_image"><img src="img/rocketlogo.png" alt=""></div>
+</div>
+<!-- Footer -->
+<footer id="footer">
+    <div class="inner">
+        <div class="copyright">
+            &copy; NINJASTORE
+            <p class="email-contato">contato@ninjastore.tk</p>
+        </div>
+</footer>
+
+   <script type="text/javascript">
+    (function(){function $MPC_load(){window.$MPC_loaded !== true && (function(){var s = document.createElement("script");s.type = "text/javascript";s.async = true;s.src = document.location.protocol+"//secure.mlstatic.com/mptools/render.js";var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);window.$MPC_loaded = true;})();}window.$MPC_loaded !== true ? (window.attachEvent ?window.attachEvent('onload', $MPC_load) : window.addEventListener('load', $MPC_load, false)) : null;})();
+</script>
+
+
+</body>
+</html>
